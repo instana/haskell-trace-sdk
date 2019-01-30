2e9c36776fb6f849cea772b9a18884dd8342da4a
Bastian Krol <bastian.krol@web.de>
2019-01-30 19:46:55 +0100

    fix(discovery): actually use host/port from agent host lookup

--- src/Instana/SDK/Internal/AgentConnection/Announce.hs
+++ src/Instana/SDK/Internal/AgentConnection/Announce.hs
@@ -26,7 +26,6 @@ import           Instana.SDK.Internal.AgentConnection.Json.AnnounceResponse (Ann
 import           Instana.SDK.Internal.AgentConnection.Paths
 import           Instana.SDK.Internal.AgentConnection.ProcessInfo           (ProcessInfo)
 import qualified Instana.SDK.Internal.AgentConnection.ProcessInfo           as ProcessInfo
-import qualified Instana.SDK.Internal.Config                                as InternalConfig
 import           Instana.SDK.Internal.Context                               (ConnectionState (..),
                                                                              InternalContext)
 import qualified Instana.SDK.Internal.Context                               as InternalContext
@@ -42,16 +41,34 @@ announce ::
   -> IO ()
 announce context processInfo = do
   debugM instanaLogger "Starting to announce process to agent."
+  connectionState <-
+      STM.atomically $ STM.readTVar $ InternalContext.connectionState context
+  case connectionState of
+    Unannounced hostAndPort ->
+      announceToAgent context hostAndPort processInfo
+    _ -> do
+      warningM instanaLogger $
+        "Reached illegal state in announce, agent host lookup did not " ++
+        "yield a host. Connection state is " ++ show connectionState ++
+        ". Will retry later."
+      STM.atomically $ STM.writeTVar
+        (InternalContext.connectionState context)
+        Unconnected
+      return ()
+
+
+announceToAgent ::
+  InternalContext
+  -> (String, Int)
+  -> ProcessInfo
+  -> IO ()
+announceToAgent context (host, port) processInfo = do
   fileDescriptor <-
-      STM.atomically $ STM.readTVar $ InternalContext.fileDescriptor context
+    STM.atomically $ STM.readTVar $ InternalContext.fileDescriptor context
   let
     manager = InternalContext.httpManager context
-    config = InternalContext.config context
     discoveryUrl =
-      URL.mkHttp
-        (InternalConfig.agentHost config)
-        (InternalConfig.agentPort config)
-        haskellDiscoveryPath
+      URL.mkHttp host port haskellDiscoveryPath
     nonContainerPid = ProcessInfo.pidString processInfo
     (pidStr, pidFromParentNS) =
        case ProcessInfo.parentNsPid processInfo of
