05adeb612b30ebd8a1c7914c690394f52d4854e2
Bastian Krol <bastian.krol@web.de>
2019-05-08 07:50:45 +0200

    chore: version 0.3.0.0

--- test/integration/Instana/SDK/IntegrationTest/Metrics.hs
+++ test/integration/Instana/SDK/IntegrationTest/Metrics.hs
@@ -51,7 +51,7 @@ shouldReportMetrics pid =
               (EntityDataRequest.arguments entityData)
           , assertLabelIs
               "sensorVersion"
-              "0.2.0.0"
+              "0.3.0.0"
               (EntityDataRequest.sensorVersion entityData)
           , assertCounterSatisfies
               "startTime"
