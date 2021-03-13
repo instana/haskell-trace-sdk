eedec30f618ced14f3cfadd08f4d00df2fd7c180
Bastian Krol <bastian.krol@instana.com>
2021-03-13 12:27:37 +0100

    chore: fix docs upload
    [skip ci]

--- bin/build-and-upload-docs.sh
+++ bin/build-and-upload-docs.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -e
 
-# Source: https://github.com/phadej/binary-orphans/blob/master/hackage-docs.sh
-
-# This if stack-enabled fork of https://github.com/ekmett/lens/blob/master/scripts/hackage-docs.sh
-
-if [ "$#" -ne 1 ]; then
-  echo "Usage: bin/build-and-upload-docs.sh HACKAGE_USER"
-  exit 1
-fi
-
-user=$1
+# Taken from https://hackage.haskell.org/upload -> Notes
 
 cabal_file=$(find . -maxdepth 1 -name "*.cabal" -print -quit)
 if [ ! -f "$cabal_file" ]; then
@@ -18,67 +9,18 @@ if [ ! -f "$cabal_file" ]; then
   exit 1
 fi
 
-pkg=$(awk -F ":[[:space:]]*" 'tolower($1)=="name"    { print $2 }' < "$cabal_file")
-ver=$(awk -F ":[[:space:]]*" 'tolower($1)=="version" { print $2 }' < "$cabal_file")
-
-if [ -z "$pkg" ]; then
-  echo "Unable to determine package name"
-  exit 1
-fi
-
-if [ -z "$ver" ]; then
-  echo "Unable to determine package version"
-  exit 1
-fi
-
-echo "Detected package: $pkg-$ver"
-
 dir=$(mktemp -d build-docs.XXXXXX)
 trap 'rm -r "$dir"' EXIT
 
 export PATH=$(stack path --bin-path)
 
 # Only installing stack will not suffice because it does not have the cabal
-# executable.
+# executable?!?
 # Use https://www.haskell.org/ghcup/ to install everything you need.
 ghc --version
 cabal --version
-stack --version
 
-if haddock --hyperlinked-source >/dev/null
-then
-  echo "Using fancy hyperlinked source"
-  HYPERLINK_FLAG="--haddock-option=--hyperlinked-source"
-else
-  echo "Using boring hyperlinked source"
-  HYPERLINK_FLAG="--hyperlink-source"
-fi
+cabal v2-haddock --builddir="$dir" --haddock-for-hackage --enable-doc
 
-# Cabal dist in temporary location
-builddir=$dir/dist
-
-# Build dependencies haddocks with stack, so we get links
-stack haddock --only-dependencies
-
-# Configure using stack databases
-snapshotpkgdb=$(stack path --snapshot-pkg-db)
-localpkgdb=$(stack path --local-pkg-db)
-cabal configure -v2 --builddir=$builddir --package-db=clear --package-db=global --package-db=$snapshotpkgdb --package-db=$localpkgdb
-
-# Build Hadckage compatible docs
-cabal haddock -v2 --builddir=$builddir $HYPERLINK_FLAG --haddock-html-location="/package/$pkg-$ver/docs" --haddock-contents-location="/package/$pkg-$ver"
-
-# Copy into right directory
-cp -R $builddir/doc/html/$pkg/ $dir/$pkg-$ver-docs
-
-# Tar and gzip
-tar cvz -C $dir --format=ustar -f $dir/$pkg-$ver-docs.tar.gz $pkg-$ver-docs
-
-# Upload
-curl -X PUT \
-     -H 'Content-Type: application/x-tar' \
-     -H 'Content-Encoding: gzip' \
-     -u "$user" \
-     --data-binary "@$dir/$pkg-$ver-docs.tar.gz" \
-     "https://hackage.haskell.org/package/$pkg-$ver/docs"
+cabal upload -d --publish $dir/*-docs.tar.gz
 
